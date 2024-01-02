05a4102aca42dbc5f57c108e851b098f22bf856f
fltzr <josh.salinas.j@outlook.com>
2024-01-01 23:18:40 -0500

    Add HealthRoutes to server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import { App } from '@/app';
 import { AuthRoute } from '@/features/auth/auth.routes';
 import { ValidateEnv } from '@/core/utils/validate-env';
 import { BudgetRoute } from '@/features/budget/budget.routes';
+import { HealthRoutes } from './features/health/health.routes';
 
 ValidateEnv();
 
-const app = new App([new AuthRoute(), new BudgetRoute()]);
+const app = new App([new AuthRoute(), new BudgetRoute(), new HealthRoutes()]);
 
 app.listen();
