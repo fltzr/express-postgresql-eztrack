7b0d88cf105c3e0b9edd265b35061f895e36f116
fltzr <josh.salinas.j@outlook.com>
2024-01-01 23:18:57 -0500

    Refactor error middleware to include additional error information

--- src/core/middlewares/error.middleware.ts
+++ src/core/middlewares/error.middleware.ts
@@ -11,10 +11,23 @@ export const ErrorMiddleware = (
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
+    const stack = error.stack;
     const errors = error.errors || {};
 
+    const errorInfo = {
+      method: request.method,
+      path: request.path,
+      status,
+      message,
+      stack: status === 500 ? stack : undefined,
+      ip: request.ip,
+      userAgent: request.get('User-Agent'),
+    };
+
     logger.error(
-      `[${request.method}] ${request.path} >> StatusCode:: ${status}, Message:: ${message}`,
+      `[${request.method}] ${errorInfo.ip} ${errorInfo.path} >> StatusCode:: ${
+        errorInfo.status
+      }, Message:: ${errorInfo.message}\n${errorInfo.stack ?? ''}`,
     );
     response.status(status).json({
       message,
